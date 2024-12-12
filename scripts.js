b1811e2dadb3d218bbed497bc2c2144e48e5b40f
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-12-12 12:14:46 -0500

    Rough in the JS to display books from library.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 body = document.querySelector("body");
 body.style.color = "white";
 
+cardContainer = document.querySelector(".card-container");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -23,3 +25,42 @@ addBookToLibrary("Seveneves", "Neal Stephenson", 867, true);
 addBookToLibrary("A Thousand Splendid Suns", "Khaled Hosseini", 370, false);
 
 console.table(myLibrary);
+
+function drawLibrary() {
+  // Loop through the Library array and create cards for each book
+  // append each book to the cardContainer
+  for(const book of myLibrary) {
+    // create the book div
+    let card = document.createElement("div");
+    card.className = "card";
+
+    let title = document.createElement("h3");
+    title.className = "title";
+    title.textContent = book.title;
+
+    let author = document.createElement("p");
+    author.className = "author";
+    author.textContent = book.author;
+    
+    let pages = document.createElement("p");
+    pages.className = "pages";
+    pages.textContent = `${book.pages} pages`;
+
+    let read = document.createElement("p");
+    read.className = "read";
+    read.textContent = book.read;
+
+    // create the title, author, page & read elements
+    // put the content of each book into the correct element
+    // append the elements to the book
+    // append the book tot he cardContainer
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    cardContainer.appendChild(card);
+  }
+}
+
+drawLibrary();
