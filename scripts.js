690a70e3e4b6cbaed789751acff32704bbbf48f9
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-12-12 09:56:21 -0500

    Create the library array & book constructor. Create some sample books.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,25 @@
 body = document.querySelector("body");
 body.style.color = "white";
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+addBookToLibrary("Leviathan's Wake", "James S. A. Corey", 561, true);
+addBookToLibrary("Seveneves", "Neal Stephenson", 867, true);
+addBookToLibrary("A Thousand Splendid Suns", "Khaled Hosseini", 370, false);
+
+console.table(myLibrary);
